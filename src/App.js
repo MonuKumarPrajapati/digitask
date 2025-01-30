6378cdff9b13f7508cdaac92e41f93df7efc4988
Monu <monuprajapati3882@gmail.com>
2025-01-30 20:50:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './global component/Header';
+import Footer from './global component/Footer';
+import Homepage from './pages/Homepage/Homepage';
+import ContactUs from './pages/contact page/ContactUs'; 
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          {/* Add other routes here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  
+    
+  );
+}
+
+export default App;
